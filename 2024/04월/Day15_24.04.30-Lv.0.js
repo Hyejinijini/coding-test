6dc447eb1c8216d018ed85c0876625e625e63cbb
Hyejinijini <qkdgpwls001@gmail.com>
2024-04-30 19:02:24 +0900

    [Level 0]_배열에서 문자열 대소문자 변환하기
    24.04.30-Lv.0: 배열에서 문자열 대소문자 변환하기 문제 풀이

--- 2024/04월/Day15_24.04.30-Lv.0.js
+++ 2024/04월/Day15_24.04.30-Lv.0.js
@@ -5,3 +5,20 @@
 function solution(myString){
   return myString.toLowerCase().replaceAll("a", "A");
 }
+
+
+// 24-04-30 (화)
+/*
+  2. 배열에서 문자열 대소문자 변환하기
+*/
+function solution(strArr) {
+    
+  for (let i = 0; i < strArr.length; i++) {
+      if (i % 2 === 1) {
+          strArr[i] = strArr[i].toUpperCase();
+      } else if (i % 2 === 0) {
+          strArr[i] = strArr[i].toLowerCase();
+      }
+  }
+  return strArr;
+}
