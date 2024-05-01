0c3d9b5e19061759ec8ac9d6b637ec7fe0a31d96
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-01 11:23:05 +0900

    [Level 0]_n 번째 원소까지
    24.05.01-Lv.0: n 번째 원소까지 문제 풀이

--- 2024/05월/Day16_24.05.01-Lv.0.js
+++ 2024/05월/Day16_24.05.01-Lv.0.js
@@ -12,3 +12,18 @@ function solution(names) {
   return answer;
 }
 
+
+// 24-05-01 (수)
+/*
+  2. n 번째 원소까지
+*/
+function solution(num_list, n) {
+  let answer = [];
+  
+  for (let i = 0; i < n; i++) {
+      answer.push(num_list[i]);
+  }
+  
+  return answer;
+}
+
