6fe23a7b2f9aad8529949f6ea6a88d5d012125f7
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-02 18:12:18 +0900

    [Level 0]_카운트 다운
    24.05.02-Lv.0: 카운트 다운 문제 풀이

--- 2024/05월/Day17_24.05.02-Lv.0.js
+++ 2024/05월/Day17_24.05.02-Lv.0.js
@@ -12,3 +12,21 @@ function solution(num_list) {
   return -1;
 }
 
+
+// 24-05-02 (목)
+/*
+  2. 카운트 다운
+*/
+function solution(start_num, end_num) {
+  let answer = [];
+  
+  for (let i = end_num; i <= start_num; i++) {
+      answer.push(i);
+  }
+  answer.sort(function (a, b) {
+      return b - a;
+  })
+
+  return answer;
+}
+
