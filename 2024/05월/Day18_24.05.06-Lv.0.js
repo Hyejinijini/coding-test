2da705f963fb50b9787e833921c461d7b9ba2fdc
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-06 16:14:43 +0900

    [Level 0]_문자열의 뒤의 n글자
    24.05.06-Lv.0: 문자열의 뒤의 n글자 문제 풀이

--- 2024/05월/Day18_24.05.06-Lv.0.js
+++ 2024/05월/Day18_24.05.06-Lv.0.js
@@ -6,3 +6,12 @@ function solution(my_string, n) {
   return my_string.substring(0, n);
 }
 
+
+// 24-05-06 (월)
+/*
+  2. 문자열의 뒤의 n글자
+*/
+function solution(my_string, n) {
+  return my_string.slice(-n);
+}
+
