282f78256b78a13689c3a9f35d1f352a0162c2b5
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-06 16:15:01 +0900

    [Level 0]_수 조작하기 1
    24.05.06-Lv.0: 수 조작하기 1 문제 풀이

--- 2024/05월/Day18_24.05.06-Lv.0.js
+++ 2024/05월/Day18_24.05.06-Lv.0.js
@@ -15,3 +15,19 @@ function solution(my_string, n) {
   return my_string.slice(-n);
 }
 
+
+// 24-05-06 (월)
+/*
+  3. 수 조작하기 1
+*/
+function solution(n, control) {
+  for (let i = 0; i < control.length; i++) {
+      switch (control[i]) { 
+          case "w": n ++; break;
+          case "s": n --; break;
+          case "d": n += 10; break;
+          case "a": n -= 10; break;   
+      }
+  }    
+  return n;
+}
