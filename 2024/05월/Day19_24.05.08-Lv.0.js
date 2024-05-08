f3ffd04b1ed42a3ae61f426ec26b78e749bb976a
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-08 19:19:57 +0900

    [Level 0]_홀짝에 따라 다른 값 반환하기
    24.05.08-Lv.0: 홀짝에 따라 다른 값 반환하기 문제 풀이

--- 2024/05월/Day19_24.05.08-Lv.0.js
+++ 2024/05월/Day19_24.05.08-Lv.0.js
@@ -17,3 +17,23 @@ function solution(num_list) {
   }
 }
 
+// 24-05-08 (수)
+/*
+  2. 홀짝에 따라 다른 값 반환하기
+*/
+function solution(n) {
+  if (n % 2 === 1) {
+      let oddSum = 0;
+      for (let i = 1; i <= n; i+=2) {
+          oddSum += i;
+      }
+      return oddSum;
+  } else if (n % 2 === 0) {
+      let evenSum = 0;
+      for (let j = 2; j <= n; j+=2) {
+          evenSum += j**2;
+      }
+      return evenSum;
+  }
+}
+
