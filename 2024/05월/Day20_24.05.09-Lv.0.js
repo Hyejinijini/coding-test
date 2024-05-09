77aa0542028c7f5e16cc012fb33b166fdc67ccea
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-09 17:05:52 +0900

    [Level 0]_두 수의 연산값 비교하기
    24.05.09-Lv.0: 두 수의 연산값 비교하기 문제 풀이

--- 2024/05월/Day20_24.05.09-Lv.0.js
+++ 2024/05월/Day20_24.05.09-Lv.0.js
@@ -6,3 +6,20 @@ function solution(num, n) {
   return (num % n === 0) ? 1 : 0;
 }
 
+
+// 24-05-09 (목)
+/*
+  2. 두 수의 연산값 비교하기
+*/
+function solution(a, b) {
+  let str = String(a) + String(b);
+  
+  if(str > (2 * a * b)) {
+      return Number(str);        
+  } else if (str < (2 * a * b)) {
+      return (2 * a * b);
+  } else {
+      return Number(str);
+  }
+}
+
