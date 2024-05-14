9d7bf2a7742c9808771e2ef1e8b5071424045292
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-14 18:27:47 +0900

    [Level 0]_배열의 원소 삭제하기
    24.05.14-Lv.0: 배열의 원소 삭제하기 문제 풀이

--- 2024/05월/Day23_24.05.14-Lv.0.js
+++ 2024/05월/Day23_24.05.14-Lv.0.js
@@ -5,3 +5,11 @@
 function solution(my_string) {
   return my_string.split(" ").filter(v=>v !== "");
 }
+
+// 24-05-14 (화)
+/*
+  2. 배열의 원소 삭제하기
+*/
+function solution(arr, delete_list) {
+  return arr.filter(v => !delete_list.includes(v));
+}
