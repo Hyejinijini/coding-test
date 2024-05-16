011dec1b9596fefd018ace88b5e1c994969eb5da
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-17 04:46:27 +0900

    [Level 0]_가까운 1 찾기
    24.05.17-Lv.0: 가까운 1 찾기 문제 풀이

--- 2024/05월/Day25_24.05.17-Lv.0.js
+++ 2024/05월/Day25_24.05.17-Lv.0.js
@@ -5,3 +5,16 @@
 function solution(arr) {
   return arr.join("");
 }
+
+// 24-05-17 (금)
+/*
+  2. 가까운 1 찾기
+*/
+function solution(arr, idx) {
+  for(let i = idx; i < arr.length; i++) {
+    if (arr[i] == 1) {
+      return i;
+    }   
+  }
+  return -1;
+}
