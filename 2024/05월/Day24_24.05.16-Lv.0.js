1a2896aeaf577b7ccf7687dde27331218c87248f
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-16 19:21:29 +0900

    [Level 0]_주사위 게임 2
    24.05.16-Lv.0: 주사위 게임 2 문제 풀이

--- 2024/05월/Day24_24.05.16-Lv.0.js
+++ 2024/05월/Day24_24.05.16-Lv.0.js
@@ -28,3 +28,19 @@ function solution(myString) {
 
   return newArr;
 }
+
+// 24-05-16 (목)
+/*
+  3. 주사위 게임 2
+*/
+function solution(a, b, c) {
+    
+  if (a!==b && a!==c && b!==c) {
+      return a+b+c;
+  } else if (a===b && a===c && b===c) {
+      return (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3);
+  } else {
+      return (a+b+c)*(a**2+b**2+c**2);
+  }
+
+}
