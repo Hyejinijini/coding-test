28884af9660ddb494cedbbbbc9f71b58f54d6ccc
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-21 19:16:39 +0900

    [Level 0]_빈 배열에 추가, 삭제하기
    24.05.21-Lv.0: 빈 배열에 추가, 삭제하기 문제 풀이

--- 2024/05월/Day27_24.05.21-Lv.0.js
+++ 2024/05월/Day27_24.05.21-Lv.0.js
@@ -5,3 +5,23 @@
 function solution(date1, date2) {
   return new Date(date1) < new Date(date2) ? 1 : 0;
 }
+
+// 24-05-21 (화)
+/*
+  2. 빈 배열에 추가, 삭제하기
+*/
+function solution(arr, flag) {
+  let answer = [];
+  
+  for (let i = 0; i < flag.length; i++) {
+    if (flag[i]) {
+      for (let j = 0; j < arr[i]*2; j++) {
+        answer.push(arr[i]);
+      }
+    } else {
+      answer.splice(-arr[i], arr[i])
+    }
+  }
+  
+  return answer;
+}
