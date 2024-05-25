1f9a988e846e557a81aa2e2e4d3e955f9fd6ed3f
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-25 17:17:32 +0900

    [Level 0]_문자열 붙여서 출력하기
    24.05.25-Lv.0: 문자열 붙여서 출력하기 문제 풀이

--- 2024/05월/Day30_24.05.25-Lv.0.js
+++ 2024/05월/Day30_24.05.25-Lv.0.js
@@ -81,3 +81,24 @@ rl.on('line', function (line) {
   console.log(`${a} + ${b} = ${a+b}`);
 });
 
+
+//24-05-25 (토)
+/*
+  5. 문자열 붙여서 출력하기
+*/
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input = line.split(' ');
+}).on('close', function () {
+  str1 = input[0];
+  str2 = input[1];
+  console.log(str1+str2);
+});
+
