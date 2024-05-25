56a7a814cc048f8b399dc654b7ff7113ca069048
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-25 17:15:37 +0900

    [Level 0]_a와 b 출력하기
    24.05.25-Lv.0: a와 b 출력하기 문제 풀이

--- 2024/05월/Day30_24.05.25-Lv.0.js
+++ 2024/05월/Day30_24.05.25-Lv.0.js
@@ -17,3 +17,25 @@ rl.on('line', function (line) {
   console.log(str);
 });
 
+
+// 24-05-25 (토)
+/*
+  2. a와 b 출력하기
+*/
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input = line.split(' ');
+}).on('close', function () {
+  const a = input[0];
+  const b = input[1];
+  console.log(`a = ${a}`);
+  console.log(`b = ${b}`);
+});
+
