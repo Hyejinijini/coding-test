1f6ac955915e9989a1f958251e6e0be37e0a9593
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-25 17:16:16 +0900

    [Level 0]_문자열 반복해서 출력하기
    24.05.25-Lv.0: 문자열 반복해서 출력하기 문제 풀이

--- 2024/05월/Day30_24.05.25-Lv.0.js
+++ 2024/05월/Day30_24.05.25-Lv.0.js
@@ -39,3 +39,24 @@ rl.on('line', function (line) {
   console.log(`b = ${b}`);
 });
 
+
+// 24-05-25 (토)
+/*
+  3. 문자열 반복해서 출력하기
+*/
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input = line.split(' ');
+}).on('close', function () {
+  str = input[0];
+  n = Number(input[1]);
+  console.log(str.repeat(n));
+});
+
