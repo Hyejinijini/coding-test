3d70ae1aaf393183e0cd62fe0934c0f310a60ac1
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-25 17:16:52 +0900

    [Level 0]_덧셈식 출력하기
    24.05.25-Lv.0: 덧셈식 출력하기 문제 풀이

--- 2024/05월/Day30_24.05.25-Lv.0.js
+++ 2024/05월/Day30_24.05.25-Lv.0.js
@@ -60,3 +60,24 @@ rl.on('line', function (line) {
   console.log(str.repeat(n));
 });
 
+
+// 24-05-25 (토)
+/*
+  4. 덧셈식 출력하기
+*/
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input = line.split(' ');
+}).on('close', function () {
+  const a = Number(input[0]);
+  const b = Number(input[1]);
+  console.log(`${a} + ${b} = ${a+b}`);
+});
+
