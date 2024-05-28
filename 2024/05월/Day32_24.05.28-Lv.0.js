27ceeee2d3df6d5bf01fcacb31a9245c53f96bb6
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-28 18:01:43 +0900

    [Level 0]_숫자 비교하기
    24.05.28-Lv.0: 숫자 비교하기 문제 풀이

--- 2024/05월/Day32_24.05.28-Lv.0.js
+++ 2024/05월/Day32_24.05.28-Lv.0.js
@@ -6,3 +6,12 @@ function solution(num1, num2) {
   return parseInt((num1 / num2)*1000);
 }
 
+
+// 24-05-28 (화)
+/*
+  2. 숫자 비교하기
+*/
+function solution(num1, num2) {   
+  return (num1 === num2) ? 1 : -1;
+}
+
