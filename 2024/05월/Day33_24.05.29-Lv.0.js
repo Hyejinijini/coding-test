0e2bb6f96e5defdb413ea953bb165911dedd47b1
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-29 18:48:25 +0900

    [Level 0]_배열의 평균값
    24.05.29-Lv.0: 배열의 평균값 문제 풀이

--- 2024/05월/Day33_24.05.29-Lv.0.js
+++ 2024/05월/Day33_24.05.29-Lv.0.js
@@ -15,3 +15,15 @@ function solution(age) {
   return (2022 - age + 1);
 }
 
+
+// 24-05-29 (수)
+/*
+  3. 배열의 평균값
+*/
+function solution(numbers) {
+  let answer = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    answer += numbers[i];
+  }
+  return answer / numbers.length;
+}
