c029ba50515692f1edcd0dac4437c0a9dc697b40
Hyejinijini <qkdgpwls001@gmail.com>
2024-05-30 18:04:47 +0900

    [Level 0]_중앙값 구하기
    24.05.30-Lv.0: 중앙값 구하기 문제 풀이

--- 2024/05월/Day34_24.05.30-Lv.0.js
+++ 2024/05월/Day34_24.05.30-Lv.0.js
@@ -18,3 +18,22 @@ function solution(n, k) {
   return (n*12000 + ((k - parseInt(n*0.1))*2000));
 }
 
+
+// 24-05-30 (목)
+/*
+  3. 중앙값 구하기
+*/
+function solution(array) {
+  let newArr = array.sort((a, b) => a-b);
+  let answer = 0;
+  
+  for (let i = 0; i < newArr.length; i++) {
+    let le = parseInt(newArr.length / 2); 
+    if (i > le - 1) {
+      answer += newArr[i];
+      break;
+    }
+  }
+  
+  return answer;
+}
