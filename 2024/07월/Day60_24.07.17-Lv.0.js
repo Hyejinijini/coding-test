7c65dcc924223778744e15a50e25fbc01fa48af9
Hyejinijini <qkdgpwls001@gmail.com>
2024-07-17 11:14:17 +0900

    [Level 0]_배열 만들기 4
    24.07.17-Lv.0: 배열 만들기 4 문제풀이

---
+++ 2024/07월/Day60_24.07.17-Lv.0.js
@@ -0,0 +1,24 @@
+// 24-07-17 (수)
+/*
+  1. 배열 만들기 4
+*/
+function solution(arr) {
+  let stk = [];
+  let i = 0;
+
+  while (i < arr.length) {
+    if (stk.length === 0) {
+      stk.push(arr[i]);
+      i++;
+    } else {
+      if (stk[stk.length - 1] < arr[i]) {
+        stk.push(arr[i]);
+        i++;
+      } else {
+        stk.pop();
+      }
+    }
+  }
+
+  return stk;
+}
