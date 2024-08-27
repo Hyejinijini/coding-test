6ead74eb5d887cb97153d8fe5da948707c24d1d4
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-27 15:03:39 +0900

    [Level 1]_자릿수 더하기
    24.08.27-Lv.1: 자릿수 더하기 문제풀이

--- 2024/08월/Day79_24.08.27-Lv.1.js
+++ 2024/08월/Day79_24.08.27-Lv.1.js
@@ -5,3 +5,17 @@
 function solution(s) {
   return Number(s);
 }
+
+/*
+  2. 자릿수 더하기
+*/
+function solution(n) {
+  let answer = 0;
+  let numStr = String(n).split("");
+
+  for (let num of numStr) {
+    answer += Number(num);
+  }
+
+  return answer;
+}
