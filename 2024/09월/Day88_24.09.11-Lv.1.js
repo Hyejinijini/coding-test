c23c4d7b0324136c3fe66016520ef2341cbf87c1
Hyejinijini <qkdgpwls001@gmail.com>
2024-09-11 11:29:07 +0900

    [Level 1]_없는 숫자 더하기
    24.09.11-Lv.1: 없는 숫자 더하기 문제풀이

--- 2024/09월/Day88_24.09.11-Lv.1.js
+++ 2024/09월/Day88_24.09.11-Lv.1.js
@@ -11,3 +11,16 @@ function solution(x, n) {
 
   return answer;
 }
+
+/*
+  2. 없는 숫자 더하기
+*/
+function solution(numbers) {
+  let answer = 0;
+
+  for (let i = 0; i <= 9; i++) {
+    if (!numbers.includes(i)) answer += i;
+  }
+
+  return answer;
+}
