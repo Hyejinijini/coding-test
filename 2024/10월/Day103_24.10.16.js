4ce8fbcba2ba4947a2af40edd56f1b404765d1a9
HyeJin <155633287+Hyejinijini@users.noreply.github.com>
2024-10-23 17:34:42 +0900

    [Leet Code]_두 개의 합
    24.10.23: 두 개의 합 문제풀이

---
+++ 2024/10월/Day103_24.10.16.js
@@ -0,0 +1,16 @@
+// 24-10-23 (수)
+/*
+  1. 두 개의 합
+*/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) return [i, j];
+    }
+  }
+};
